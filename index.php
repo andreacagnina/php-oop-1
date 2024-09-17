eaf368b6c8fec336d92c26716f709291e9bea6fd
andreacagnina <amirkal@libero.it>
2024-09-17 19:07:26 +0200

    [feat] print

--- index.php
+++ index.php
@@ -33,22 +33,22 @@ class Movie
     public function setDurationType($typeOfDuration)
     {
         if ($typeOfDuration < 40) {
-            return $typeOfDuration = '(Cortometraggio)';
+            return $typeOfDuration = '(Cortometraggio FALSO)';
         } else if ($typeOfDuration > 40 && $typeOfDuration < 60) {
             return $typeOfDuration = '(Mediometraggio)';
         } else if ($typeOfDuration > 90 && $typeOfDuration < 180) {
             return $typeOfDuration = '(Lungometraggio)';
         }
     }
-    public function getFullName()
-    {
-        return $this->director->name . " " . $this->director->surname . " " . "(" . "nato il" . " " . $this->director->date_of_birth . ")";
-    }
     public function getAge()
     {
         $today = new DateTime();
         $age = $this->director->date_of_birth->diff($today)->y;
-        return $age;
+        return " " . "(" . $age . "anni" . ")";
+    }
+    public function getFullName()
+    {
+        return $this->director->name . " " . $this->director->surname;
     }
 }
 
@@ -57,8 +57,6 @@ class Director
     public $name;
     public $surname;
     public $date_of_birth;
-    public $gender;
-    public $nationality;
 
     function __construct($name, $surname, $date_of_birth)
     {
@@ -68,15 +66,34 @@ class Director
     }
 }
 
-$The_avengers = new Movie('The Avengers', 'Action', 2012, 10, 'Robert Downey Jr', new Director('Joss', 'Whedon', '1964/06/23'));
-$Avengers_Age_of_Ultron = new Movie('Avengers: Age of Ultron', 'Action', 2015, 141, 'James Spader', new Director('Joss', 'Whedon', '1964/06/23'));
-$Avengers_Infinity_War = new Movie('Avengers: Infinity War', 'Action', 2018, 149, 'Chris Hemsworth', new Director('Anthony', 'Russo', '1970/02/03'));
-$Avengers_End_Game = new Movie('Avengers: Endgame', 'Action', 2019, 181, 'Josh Brolin', new Director('Joe', 'Russo', '1971/07/18'));
+$The_avengers = new Movie('The Avengers', 'Action', 2012, 10, 'Robert Downey Jr', new Director('Joss', 'Whedon', new DateTime('1964-06-23')));
+$Avengers_Age_of_Ultron = new Movie('Avengers: Age of Ultron', 'Action', 2015, 141, 'James Spader', new Director('Joss', 'Whedon', new DateTime('1964-06-23')));
+$Avengers_Infinity_War = new Movie('Avengers: Infinity War', 'Action', 2018, 149, 'Chris Hemsworth', new Director('Anthony', 'Russo', new DateTime('1970-02-03')));
+$Avengers_End_Game = new Movie('Avengers: Endgame', 'Action', 2019, 181, 'Josh Brolin', new Director('Joe', 'Russo', new DateTime('1971-07-18')));
 
 $The_avengers->getStreamingMessage();
 $The_avengers->getLanguage();
 $The_avengers->getFullName();
 $The_avengers->setDurationType('duration');
+$The_avengers->getAge();
+
+$Avengers_Age_of_Ultron->getStreamingMessage();
+$Avengers_Age_of_Ultron->getLanguage();
+$Avengers_Age_of_Ultron->getFullName();
+$Avengers_Age_of_Ultron->setDurationType('duration');
+$Avengers_Age_of_Ultron->getAge();
+
+$Avengers_Infinity_War->getStreamingMessage();
+$Avengers_Infinity_War->getLanguage();
+$Avengers_Infinity_War->getFullName();
+$Avengers_Infinity_War->setDurationType('duration');
+$Avengers_Infinity_War->getAge();
+
+$Avengers_End_Game->getStreamingMessage();
+$Avengers_End_Game->getLanguage();
+$Avengers_End_Game->getFullName();
+$Avengers_End_Game->setDurationType('duration');
+$Avengers_End_Game->getAge();
 
 
 ?>
@@ -136,10 +153,141 @@ $The_avengers->setDurationType('duration');
                             </li>
                             <li>
                                 <h3>
-                                    Regista: <?php echo $The_avengers->getFullName() ?>
+                                    Regista: <?php echo $The_avengers->getFullName(), $The_avengers->getAge() ?>
                                 </h3>
                             </li>
                         </ul>
+                        <HR>
+                        </HR>
+                        <ul>
+                            <li>
+                                <h2>
+                                    <?php echo $Avengers_Age_of_Ultron->title ?>
+                                </h2>
+                            </li>
+                            <li>
+                                <h3>
+                                    Genere: <?php echo $Avengers_Age_of_Ultron->genre ?>
+                                </h3>
+                            </li>
+                            <li>
+                                <h3>
+                                    Anno di Uscita: <?php echo $Avengers_Age_of_Ultron->year ?>
+                                </h3>
+                            </li>
+                            <li>
+                                <h3>
+                                    Titolo: <?php echo $Avengers_Age_of_Ultron->duration ?> minuti <?php echo $Avengers_Age_of_Ultron->setDurationType($Avengers_Age_of_Ultron->duration) ?>
+                                </h3>
+                            </li>
+                            <li>
+                                <h3>
+                                    Lingua Originale: <?php echo $Avengers_Age_of_Ultron->getLanguage() ?>
+                                </h3>
+                            </li>
+                            <li>
+                                <h3>
+                                    Disponibile in: <?php echo $Avengers_Age_of_Ultron->getStreamingMessage() ?>
+                                </h3>
+                            </li>
+                            <li>
+                                <h3>
+                                    Attore principale: <?php echo $Avengers_Age_of_Ultron->main_actor ?>
+                                </h3>
+                            </li>
+                            <li>
+                                <h3>
+                                    Regista: <?php echo $Avengers_Age_of_Ultron->getFullName(), $Avengers_Age_of_Ultron->getAge() ?>
+                                </h3>
+                            </li>
+                        </ul>
+                        <hr>
+                        <ul>
+                            <li>
+                                <h2>
+                                    <?php echo $Avengers_Infinity_War->title ?>
+                                </h2>
+                            </li>
+                            <li>
+                                <h3>
+                                    Genere: <?php echo $Avengers_Infinity_War->genre ?>
+                                </h3>
+                            </li>
+                            <li>
+                                <h3>
+                                    Anno di Uscita: <?php echo $Avengers_Infinity_War->year ?>
+                                </h3>
+                            </li>
+                            <li>
+                                <h3>
+                                    Titolo: <?php echo $Avengers_Infinity_War->duration ?> minuti <?php echo $Avengers_Infinity_War->setDurationType($Avengers_Infinity_War->duration) ?>
+                                </h3>
+                            </li>
+                            <li>
+                                <h3>
+                                    Lingua Originale: <?php echo $Avengers_Infinity_War->getLanguage() ?>
+                                </h3>
+                            </li>
+                            <li>
+                                <h3>
+                                    Disponibile in: <?php echo $Avengers_Infinity_War->getStreamingMessage() ?>
+                                </h3>
+                            </li>
+                            <li>
+                                <h3>
+                                    Attore principale: <?php echo $Avengers_Infinity_War->main_actor ?>
+                                </h3>
+                            </li>
+                            <li>
+                                <h3>
+                                    Regista: <?php echo $Avengers_Infinity_War->getFullName(), $Avengers_Infinity_War->getAge() ?>
+                                </h3>
+                            </li>
+                        </ul>
+                        <hr>
+                        <ul>
+                            <li>
+                                <h2>
+                                    <?php echo $Avengers_End_Game->title ?>
+                                </h2>
+                            </li>
+                            <li>
+                                <h3>
+                                    Genere: <?php echo $Avengers_End_Game->genre ?>
+                                </h3>
+                            </li>
+                            <li>
+                                <h3>
+                                    Anno di Uscita: <?php echo $Avengers_End_Game->year ?>
+                                </h3>
+                            </li>
+                            <li>
+                                <h3>
+                                    Titolo: <?php echo $Avengers_End_Game->duration ?> minuti <?php echo $Avengers_End_Game->setDurationType($Avengers_End_Game->duration) ?>
+                                </h3>
+                            </li>
+                            <li>
+                                <h3>
+                                    Lingua Originale: <?php echo $Avengers_End_Game->getLanguage() ?>
+                                </h3>
+                            </li>
+                            <li>
+                                <h3>
+                                    Disponibile in: <?php echo $Avengers_End_Game->getStreamingMessage() ?>
+                                </h3>
+                            </li>
+                            <li>
+                                <h3>
+                                    Attore principale: <?php echo $Avengers_End_Game->main_actor ?>
+                                </h3>
+                            </li>
+                            <li>
+                                <h3>
+                                    Regista: <?php echo $Avengers_End_Game->getFullName(), $Avengers_End_Game->getAge() ?>
+                                </h3>
+                            </li>
+                        </ul>
+
                     <?php } ?>
                 </div>
             </div>
